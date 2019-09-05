1fbe15f47c81d8a96fc9d59ff22752c4d04b1059
Peter Misak <peter.misak@gmail.com>
2019-09-05 23:34:10 +0200

    Initial setup.sh

---
+++ setup.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+echo Confinguring defaults
+
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# Don’t automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+# Hot corners
+# Possible values:
+#  0: no-op
+#  2: Mission Control
+#  3: Show application windows
+#  4: Desktop
+#  5: Start screen saver
+#  6: Disable screen saver
+#  7: Dashboard
+# 10: Put display to sleep
+# 11: Launchpad
+# 12: Notification Center
+# Top left screen corner → Sleep
+defaults write com.apple.dock wvous-tl-corner -int 2
+defaults write com.apple.dock wvous-tl-modifier -int 0
+defaults write com.apple.dock wvous-bl-corner -int 5
+defaults write com.apple.dock wvous-bl-modifier -int 0
+defaults write com.apple.dock wvous-tr-corner -int 4
+defaults write com.apple.dock wvous-tr-modifier -int 0
+defaults write com.apple.dock wvous-br-corner -int 3
+defaults write com.apple.dock wvous-br-modifier -int 0
+
+echo "Configuring dev tools"
+
+set -e
+
+if [[ $(xcode-select --version) ]]; then
+    echo "Xcode command tools already installed"
+else
+    echo "Installing Xcode command line tools"
+    $(xcode-select --install)
+fi
+
+

61471152010acd577079200fe893fa067274cea2
Peter Misak <peter.misak@gmail.com>
2019-09-06 21:26:45 +0200

    Set npm-global path in fish config

--- fish/config.fish
+++ fish/config.fish
@@ -7,3 +7,6 @@ set -xg PATH /opt/local/bin /opt/local/sbin $PATH
 # vscode
 set -xg PATH "/Applications/Visual Studio Code.app/Contents/Resources/app/bin" $PATH
 
+# Path to node global (the dir was created manually by mkdir ~/.npm-global ;  npm config set prefix '~/.npm-global')
+set -xg PATH ~/.npm-global/bin $PATH
+

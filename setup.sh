3b57d9b01f5097bebfa51e83ec91d84467abc926
Peter Misak <peter.misak@gmail.com>
2019-09-11 18:33:47 +0200

    Add fish plugin to vscode

--- setup.sh
+++ setup.sh
@@ -97,6 +97,7 @@ code --install-extension ccy.ayu-adaptive
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension VisualStudioExptTeam.vscodeintellicode
 code --install-extension vscode-icons-team.vscode-icons
+code --install-extension skyapps.fish-vscode
 
 ln -sf $(pwd)/prefs/vscode/settings.json "$HOME/Library/Application Support/Code/User/settings.json"
 

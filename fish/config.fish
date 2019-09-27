22b9ad541663434833af41a068c00b91e013a277
Peter Misak <peter.misak@gmail.com>
2019-09-27 11:41:58 +0200

    Fix locale

--- fish/config.fish
+++ fish/config.fish
@@ -10,3 +10,6 @@ set -xg PATH "/Applications/Visual Studio Code.app/Contents/Resources/app/bin" $
 # Path to node global (the dir was created manually by mkdir ~/.npm-global ;  npm config set prefix '~/.npm-global')
 set -xg PATH ~/.npm-global/bin $PATH
 
+# Locale fix
+set -xg LC_CTYPE en_US.UTF-8
+

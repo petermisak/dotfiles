78bbd03fbcde9088970770281836407ea63daf95
Peter Misak <peter.misak@gmail.com>
2020-05-18 16:31:49 +0200

    Change of search highlight color in fish

--- fish/config.fish
+++ fish/config.fish
@@ -56,7 +56,8 @@ set -xg PATH $HOME/bin $PATH
 #set -g fish_color_user brgreen
 #set -g fish_color_valid_path --underline
 
-set -g fish_color_search_match 'black'  '--background=brgreen'
+set -g fish_color_search_match 'black'  '--background=d8dbda'
+set -g fish_pager_color_prefix 'black' '--bold' '--underline'
 
 function _gen_fzf_default_opts
     set color00 '#1B2B34'

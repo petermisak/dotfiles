9fe90fc32f31455c4168457e13567db6aaba82db
Peter Misak <peter.misak@gmail.com>
2022-02-21 12:21:20 +0100

    Fix espanso config

--- setup.sh
+++ setup.sh
@@ -171,3 +171,6 @@ brew install weaveworks/tap/eksctl
 # See https://espanso.org/docs/install/mac
 brew tap federico-terzi/espanso
 brew install espanso
+rm -rf ~/Library/Preferences/espanso
+ln -sf $(pwd)/espanso "$HOME/Library/Preferences"
+

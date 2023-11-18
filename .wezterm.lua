e882a852a181b94927bbba05d5de6c10db5dc1e8
Peter Misak <peter.misak@gmail.com>
2023-11-18 02:31:06 +0100

    Update wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,37 @@
 local wezterm = require 'wezterm'
 
+---cycle through builtin dark schemes in dark mode, 
+---and through light schemes in light mode
+local function themeCycler(window, _)
+  local allSchemes = wezterm.color.get_builtin_schemes()
+  local currentMode = wezterm.gui.get_appearance()
+  local currentScheme = window:effective_config().color_scheme
+  local darkSchemes = {}
+  local lightSchemes = {}
+
+  for name, scheme in pairs(allSchemes) do
+    local bg = wezterm.color.parse(scheme.background) -- parse into a color object
+    ---@diagnostic disable-next-line: unused-local
+    local h, s, l, a = bg:hsla() -- and extract HSLA information
+    if l < 0.4 then
+      table.insert(darkSchemes, name)
+    else
+      table.insert(lightSchemes, name)
+    end
+  end
+  local schemesToSearch = currentMode:find("Dark") and darkSchemes or lightSchemes
+
+  for i = 1, #schemesToSearch, 1 do
+    if schemesToSearch[i] == currentScheme then
+      local overrides = window:get_config_overrides() or {}
+      overrides.color_scheme = schemesToSearch[i+1]
+      wezterm.log_info("Switched to: " .. schemesToSearch[i+1])
+      window:set_config_overrides(overrides)
+      return
+    end
+  end
+end
+
 -- This table will hold the configuration.
 local config = {}
 
@@ -10,8 +42,8 @@ if wezterm.config_builder then
 end
 
 -- config.color_scheme = 'AtomOneLight'
-config.color_scheme = 'Piatto Light'
--- config.color_scheme = 'One Light (Gogh)'
+-- config.color_scheme = 'Piatto Light'
+config.color_scheme = 'One Light (base16)'
 -- config.color_scheme = 'Tomorrow'
 config.font = wezterm.font('JetBrainsMono Nerd Font', { weight = 'Medium' })
 config.font_size = 13
@@ -63,6 +95,14 @@ config.colors = {
       fg_color = '#555'
     }
   }
+}
+
+config.keys = {
+  -- Theme Cycler
+  { key = "t", mods = "ALT", action = wezterm.action_callback(themeCycler) },
+
+  -- Look up Scheme you switched to
+  { key = "Escape", mods = "CTRL", action = wezterm.action.ShowDebugOverlay },
 }
 
 return config

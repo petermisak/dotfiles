65816cdb630fe30b145ae11fee093154260b04b9
Peter Misak <peter.misak@gmail.com>
2024-07-08 16:06:25 +0200

    Config update

--- anyconnectctl.sh
+++ anyconnectctl.sh
@@ -6,17 +6,15 @@ if [ -z "$1" ]; then
 fi
 
 if [ "$1" = "off" ]; then
-	echo "Disabling vpnagentd..."
-	sudo launchctl disable system/com.cisco.anyconnect.vpnagentd
-	echo "Tearing down vpnagentd..."
+	echo "Quitting Cisco Secure Client - AnyConnect VPN Service"
+	sudo /usr/bin/osascript -e 'quit app "Cisco Secure Client - AnyConnect VPN Service.app"'
+	sudo /usr/bin/open -W -a "/opt/cisco/secureclient/bin/Cisco Secure Client - AnyConnect VPN Service.app" --args uninstall
 	sudo launchctl bootout system /Library/LaunchDaemons/com.cisco.anyconnect.vpnagentd.plist
-	echo "Deactivating Cisco AnyConnect Socket Filter Extension..."
-	/Applications/Cisco/Cisco\ AnyConnect\ Socket\ Filter.app/Contents/MacOS/Cisco\ AnyConnect\ Socket\ Filter -deactivateExt
+	echo "Deactivating Cisco Secure Client - Socket Filter Extension..."
+	sudo "/Applications/Cisco/Cisco Secure Client - Socket Filter.app/Contents/MacOS/Cisco Secure Client - Socket Filter" -deactivateExt
 elif [ "$1" = "on" ]; then
-	echo "Enabling vpnagentd..."
-	sudo launchctl enable system/com.cisco.anyconnect.vpnagentd
-	echo "Bootstrapping vpnagentd..."
-	sudo launchctl bootstrap system /Library/LaunchDaemons/com.cisco.anyconnect.vpnagentd.plist
+	echo "Starting Cisco Secure Client - AnyConnect VPN Service"
+	sudo open -a "/opt/cisco/secureclient/bin/Cisco Secure Client - AnyConnect VPN Service.app"
 else
 	echo "Invalid option. Usage: $0 [on|off]"
 	exit 1
--- fish/functions/change_background.fish
+++ fish/functions/change_background.fish
@@ -3,15 +3,15 @@ function change_background --argument mode_setting
     set -l mode light
     switch $mode_setting
         case light
-            # yes | fish_config theme save "Snow Day"
+            yes | fish_config theme save "Snow Day"
             # yes | fish_config theme save Tomorrow
             #yes | fish_config theme save "Catppuccin Latte"
-            yes | fish_config theme save "Rosé Pine Dawn"
+            #yes | fish_config theme save "Rosé Pine Dawn"
             git config --global delta.syntax-theme OneHalfLight
             set mode light
         case dark
             #yes | fish_config theme save "Catppuccin Macchiato"
-            # yes | fish_config theme save "Rosé Pine Moon"
+            #yes | fish_config theme save "Rosé Pine Moon"
             yes | fish_config theme save "Rosé Pine"
             git config --global delta.syntax-theme zenburn
             set mode dark
